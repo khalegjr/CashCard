2e0557271358e88da9ff12489d29a86ba38ff8ca
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-05-08 17:22:42 -0300

    cria teste de deserialização

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -35,4 +35,18 @@ public class CashCardJsonTest {
                 .extractingJsonPathNumberValue("@.amount")
                 .isEqualTo(123.45);
     }
+
+    @Test
+    public void cashCardDeserializationTest() throws IOException {
+        String expected = """
+           {
+               "id":99,
+               "amount":123.45
+           }
+           """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(99L, 123.45));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
 }
